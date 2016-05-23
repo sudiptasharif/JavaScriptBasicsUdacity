1c38e046e65ade5b69a1ac3737ce40ce42b55e67
sudipta <sudipta.sharif@outlook.com>
2016-05-23 13:28:27 -0400

    Add work json object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -154,24 +154,55 @@ var education = {
 */
 
 // Works
-var works = {
+var workExperience = {
 	"student_security_analyst":{
-		
+		"employer":"Indiana University Purdue University Fort Wayne - Information Technology Services",
+		"title": "Student Security Analyst",
+		"location":"Fort Wayne, IN",
+		"dates":"November 2014-Present",
+		"description":"As a student network security analyst, I am in charge of detecting and liaising threats imposed by various Malware and Spyware intended for faculty, stuff and students."+
+		"I research, investigate and respond to Spam emails, suspicious file attachments and harmful URLs that direct users to socially engineered websites. I am also in charge of"+
+		"training new employees and interviewing new potential employees for the security office."
 	},
 	"web_developer":{
-		
+		"employer":"Indiana University Purdue University Fort Wayne - Lincoln Financial Group (Partner)",
+		"title": "Team Leader - Senior Design Capstone Project",
+		"location": "Fort Wayne, IN",
+		"dates": "August 2015-May 2016",
+		"description": "As part of our senior design project, my team and I were in charge of building a fully functional web application that automated various tasks for our client Lincoln Financial Group."+
+		"This web application uses PHP and MySQL as the back end and HTML, CSS, JavaScript, JQuery, AJAX and Bootstrap as the front end technology."
+
 	},
 	"java_developer":{
-		
+		"employer": "Agri Stats Inc.",
+		"title":"Intern - Java Developer",
+		"location": "Fort Wayne, IN",
+		"dates": "May 2015-August 2015",
+		"description":"As an intern for Agri Stats Inc., I was responsible for converting clipper codes to Java using Agri Stat's Clipper-Java API. I also inserted Java functions to"+
+		"a PostgreSQL database, tested the accuracy of converted functions by looking at generated logs and debugged the converted Java code whenever necessary."
 	},
 	"lab_response_team":{
-		
+		"employer":"Indiana University Purdue University Fort Wayne - Information Technology Services",
+		"title": "Lab Consultant - Part of the Lab Response Team (LRT)",
+		"location": "Fort Wayne",
+		"dates": "July 2013-November 2014",
+		"description": "As part of this team, I provided technical support to faculty and student with any issues on their laptops, phones and tables or school comouters with respect to"+
+		"IPFW's applications and devices such as wifi connections, printer issues, IU ware software downloads, any help with Microsoft Word, Excel and Visio. I communicated any device"+
+		"that needed replacement or repaired with IPFW Library technicians."
 	},
 	"math_tutor":{
-		
+		"employer":"Indiana University Purdue University Fort Wayne - Information Technology Services",
+		"title": "Mathematics Tutor",
+		"location": "Fort Wayne, IN",
+		"dates": "February 2013-July 2013",
+		"description":"As a mathematics tutor, I helped students with homework, quizzes and exams. I provided conceptual understanding of the following topics Calculus, Discrete math, Linear algebra and Statistics."
 	},
 	"teaching_assistant":{
-		
+		"employer":"Indiana University Purdue University Fort Wayne - Biology Department",
+		"title": "Teaching Assistant",
+		"location": "Fort Wayne, IN",
+		"dates": "October 2012-July 2013",
+		"description": "I helped my Biology professor with grading homeworks, quizzes and exams. I was also in charge of uploading grades to Blackboard."
 	}
 };
 
@@ -181,9 +212,6 @@ var projects = {
 		
 	},
 	"vulnerable_web_app":{
-		
-	},
-	"job_application_logger":{
 		
 	}
 };
@@ -207,3 +235,6 @@ var educations = {
 		
 	}
 };
+
+
+
